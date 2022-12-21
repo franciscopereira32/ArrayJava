9450e5d3dd566e57ff06c2e71fd4b6cfd20635ab
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2022-12-21 15:35:16 +1030

    add repeating structure

--- Array.java
+++ Array.java
@@ -1,7 +1,12 @@
 public class Array{
     public static void main(String args[]){
         String[] names = {"Pedro", "Viviane", "Francisco"};
-        System.out.println(names[0]);
+        
+        for (int i=0; i<3; i++){
+            System.out.println(names[i]);
+        }
+        System.out.println("Fim do Array");
+        
     }
     
 }
