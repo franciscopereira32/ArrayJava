2908c55a521e09afe0061c2f32dd7ace3156e1e4
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2022-12-21 14:48:31 +1030

    first commit

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array{
+    public static void main(String args[]){
+        String[] names = {"Pedro", "Viviane", "Francisco"};
+        System.out.println(names[0]);
+    }
+    
+}
+
+
