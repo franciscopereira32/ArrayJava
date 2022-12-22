01718a058dcfff6a53d7a91903be605816aa9e99
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2022-12-22 18:27:27 +1030

    getting input

--- Array.java
+++ Array.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Array{
+    private static Scanner s;
     public static void main(String args[]){
-        String[] names = {"Pedro", "Viviane", "Francisco"};
         
-        for (int i=0; i<3; i++){
-            System.out.println(names[i]);
+        s = new Scanner(System.in);
+        
+        ArrayList<Times> listaTimes = new ArrayList<Times>();
+
+        for (int i=1; i<5; i++){
+            Times time = new Times();
+            System.out.println("Digite um time: ");
+            time.nome = s.next();    
+            listaTimes.add(time);
         }
-        System.out.println("Fim do Array");
+        
+        System.out.println(listaTimes);
         
     }
     
---
+++ Times.java
@@ -0,0 +1,20 @@
+
+public class Times {
+
+    String nome;
+
+        public Times(String nome){
+            this.nome = nome;
+        }
+        public Times() {
+        }
+        public String  getNome(){
+            return nome;
+        }
+        public void setNome(String time) {
+            this.nome=nome;
+        }
+        public String toString(){
+            return nome + " ";
+        }
+}
