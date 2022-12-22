173b34a4bc801dcd4773098a158608bae7652154
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2022-12-22 18:35:36 +1030

    With JOptionPane and drop Scanner

--- Array.java
+++ Array.java
@@ -1,23 +1,18 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class Array{
-    private static Scanner s;
     public static void main(String args[]){
-        
-        s = new Scanner(System.in);
-        
+                
         ArrayList<Times> listaTimes = new ArrayList<Times>();
 
         for (int i=1; i<5; i++){
             Times time = new Times();
-            System.out.println("Digite um time: ");
-            time.nome = s.next();    
+            time.nome = JOptionPane.showInputDialog("Digite o nome do time" );    
             listaTimes.add(time);
         }
-        
+        JOptionPane.showMessageDialog(null, listaTimes);
         System.out.println(listaTimes);
         
     }
